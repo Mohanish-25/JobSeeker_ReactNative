2476d50099fc744e5ba252717634630fe0d09cfa
Kapil619 <kapilbadokar321@gmail.com>
2024-04-26 00:05:16 +0530

    Installed async storage, Persisting auth state , Issue of the auth page appearing while login.

--- app/_layout.js
+++ app/_layout.js
@@ -1,5 +1,9 @@
 import { Stack } from "expo-router";
 import { useFonts } from "expo-font";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { ActivityIndicator } from "react-native";
 // import * as SplashScreen from "expo-splash-screen";
 
 // SplashScreen.preventAutoHideAsync();
@@ -16,8 +20,28 @@ const Layout = () => {
     DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
   });
 
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const auth = getAuth();
+
+    // Listen for auth state changes
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, navigate to the main app screen
+        navigation.navigate("home");
+      } else {
+        // User is signed out, navigate to the auth screen
+        navigation.navigate("auth");
+      }
+    });
+
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  }, [navigation]);
+
   if (!fontsLoaded) {
-    return null;
+    return <ActivityIndicator size="large" color={"#fff"} />;
   }
 
   return (
